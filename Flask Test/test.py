87911715c8f1690c3a77def38b43f19ebdf2b81d
Eric Oswald <eoswald39@gmail.com>
2014-09-07 20:46:09 -0400

    Made jQuery example for getting random number pushed to server

--- Flask Test/test.py
+++ Flask Test/test.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template, request
 app = Flask( __name__ )
 
-@app.route( "/" )
-def getRandom():
+@app.route( '/_random_number' )
+def get_random():
 	with open( "/dev/urandom", 'rb' ) as f:
-		return repr( ord( f.read( 1 ) ) )
+		return jsonify(  result=ord( f.read( 1 ) ) )
 
-@app.route( "/hello" )
-def getJorel():
-	return "Jorel"
+@app.route( '/' )
+def index():
+	return render_template( 'index.html' )
 
 if __name__ == "__main__":
+	app.debug = True
 	app.run()
