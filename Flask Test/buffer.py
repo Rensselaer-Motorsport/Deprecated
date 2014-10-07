5c04952d3586819ef840157dc0e2cce6b5fbc297
Eric Oswald <eoswald39@gmail.com>
2014-10-07 19:37:34 -0400

    Fixed bugs in buffer class.

--- Flask Test/buffer.py
+++ Flask Test/buffer.py
@@ -1,39 +1,34 @@
-class Buffer(object):
+class B:
 	
 	#initializes the class.
 	#can either take in an already made sensor dictionary
 	#or it will make an empty dictionary
-	def _init_(self, pre_sensor_dict):
-		if pre_sensor_dict is None:
-			#sensor_dict is a dictionary holding other dictionarys of seperate sensors
-			self.sensor_dict = pre_sensor_dict
-		else:
-			self.sensor_dict = {}
+	def __init__(self):
+		#sensor_dict is a dictionary holding other dictionarys of seperate sensors
+		self.sensor_dict = {}
 
-	def addSensor (sensor_name):
-		sensor_dict[sensor_name] = {}
+	def addSensor (self, sensor_name):
+		self.sensor_dict[sensor_name] = []
 
-	def addNewSensorValue (sensor_name, new_value_key, new_value):
-		sensor_dict[sensor_name][new_value_key] = new_value
+	def addSensorValue (self, sensor_name, new_value):
+		self.sensor_dict[sensor_name].append(new_value)
 
-	def getSensor (sensor_name):
-		return sensor_dict.get(sensor_name)
+	def getSensor (self, sensor_name):
+		return self.sensor_dict[sensor_name]
 
-	def getSensorValue (sensor_name, value_key):
-		return sensor_dict.get(sensor_name).get(value_key)
-
+	def getSensorValue (self, sensor_name):
+		return self.sensor_dict[sensor_name].pop()
 
 #bufferTest.py
-
-sensorDict = Buffer()
+sensorDict = B()
 
 sensorDict.addSensor("engine_sensor")
-sensorDict.addSensorValue("engine", "temperature", 150)
-sensorDict.addSensorValue("engine", "oil pressure", 89)
+sensorDict.addSensorValue("engine_sensor", 150)
+sensorDict.addSensorValue("engine_sensor", 89)
 sensorDict.addSensor("suspension")
-sensorDict.addSensorValue("suspension", "force", 12)
+sensorDict.addSensorValue("suspension", 12)
 
-print("test")
-print(sensorDict.getSensorValue("engine", "temperature"))
-print(sensorDict.getSensorValue("engine", "oil pressure"))
-print(sensorDict.getSensorValue("suspension", "force"))
+print "test"
+print sensorDict.getSensorValue("engine_sensor")
+print sensorDict.getSensorValue("engine_sensor")
+print sensorDict.getSensorValue("suspension")
