26c71d26028ca0d9e1066587b9cd6328c776512b
Mitchell Mellone <mellom3@rpi.edu>
2014-10-10 20:02:36 -0400

    Added print functions for the sensorBuffer class. Also added a function to access specific data values from a sensor (getSensorValue). Commented before every function so it is clear what everything does. Added error checking to make sure sensors/data elements are in the dictionary before trying to access them.

--- Flask Test/bufferTest.py
+++ Flask Test/bufferTest.py
@@ -1,15 +1,45 @@
 import sensor_buffer
 #bufferTest.py
 
+print
+
 sensorDict = sensor_buffer.sensorBuffer()
 
+#add sensors and values
 sensorDict.addSensor("engine_sensor")
-sensorDict.addSensorValue("engine_sensor", 150)
-sensorDict.addSensorValue("engine_sensor", 89)
 sensorDict.addSensor("suspension")
-sensorDict.addSensorValue("suspension", 12)
+for x in xrange(0, 25):
+	sensorDict.addSensorValue("engine_sensor", x*(4.5))
+	sensorDict.addSensorValue("suspension", x)
 
-print("test")
-print(sensorDict.getSensorValue("engine_sensor"))
-print(sensorDict.getSensorValue("engine_sensor"))
-print(sensorDict.getSensorValue("suspension"))
+#print sensors
+print "Print the sensors with data"
+sensorDict.printSensorValues("engine_sensor")
+sensorDict.printSensorValues("suspension")
+
+#test error cases
+sensorDict.addSensor("engine_sensor")
+sensorDict.addSensorValue("bad_sensor", 5)
+sensorDict.getSensor("bad_sensor")
+sensorDict.getSensorValue("bad_sensor", 3)
+sensorDict.getSensorValue("engine_sensor", 28)
+print sensorDict.getSensor("shitwentwrong")
+
+#test other functions
+print
+print "Test other functions"
+print 
+print "Returns the engine_sensor value list"
+print sensorDict.getSensor("engine_sensor")
+print
+print sensorDict.getLastSensorValue("engine_sensor")
+print "expected 108.0"
+print
+print sensorDict.getLastSensorValue("engine_sensor")
+print "expected 103.5"
+print
+print sensorDict.getSensorValue("engine_sensor", 5)
+print "expected 22.5"
+print
+print "Print entire dictionary"
+print sensorDict.printDictionary()
--- Flask Test/sensor_buffer.py
+++ Flask Test/sensor_buffer.py
@@ -7,14 +7,91 @@ class sensorBuffer:
 		#sensor_dict is a dictionary holding other dictionarys of seperate sensors
 		self.sensor_dict = {}
 
+	#adds a new sensor to the end of the dictionary
+	#param sensor_name is the key for the sensor
 	def addSensor (self, sensor_name):
+		for key in self.sensor_dict.keys():
+			if sensor_name == key:
+				print "Sensor already added"
+				return
 		self.sensor_dict[sensor_name] = []
 
+	#adds a value to the end of a specified sensor
+	#param sensor_name is the name of sensor/dictionary key
+	#param new value is the value to be added
 	def addSensorValue (self, sensor_name, new_value):
-		self.sensor_dict[sensor_name].append(new_value)
+		found = False
+		for key in self.sensor_dict.keys():
+			if sensor_name == key:
+				found = True
+				break
+		if found:
+			self.sensor_dict[sensor_name].append(new_value)
+		else:
+			print "Sensor does not exist"
 
+	#returns the list of values for a specified sensor
+	#param sensor_name is the name of sensor/dictionary key
 	def getSensor (self, sensor_name):
-		return self.sensor_dict[sensor_name]
+		found = False
+		for key in self.sensor_dict.keys():
+			if sensor_name == key:
+				found = True
+				break
+		if found:
+			return self.sensor_dict[sensor_name]
+		else:
+			print "Sensor does not exist"
 
-	def getSensorValue (self, sensor_name):
-		return self.sensor_dict[sensor_name].pop()
+	#returns and erases the last value of a specified sensor
+	#param sensor_name is the name of sensor/dictionary key
+	def getLastSensorValue (self, sensor_name):
+		found = False
+		for key in self.sensor_dict.keys():
+			if sensor_name == key:
+				found = True
+				break
+		if not found:
+			print "Sensor does not exist"
+		else:		
+			return self.sensor_dict[sensor_name].pop()
+
+	#returns a specified value of a specified sensor
+	#does not change the sensor list
+	#param sensor_name is the name of sensor/dictionary key
+	#param value_index is the index of the value to be returned
+	def getSensorValue (self, sensor_name, value_index):
+		found = False
+		for key in self.sensor_dict.keys():
+			if sensor_name == key:
+				found = True
+				break
+		if not found:
+			print "Sensor does not exist"
+		elif value_index >= len(self.sensor_dict[sensor_name]):
+			print "Value index does not exist"
+		else:
+			return self.sensor_dict[sensor_name][value_index]
+
+	#prints all of the values of a specified sensor
+	#param sensor_name is the name of sensor/dictionary key
+	def printSensorValues (self, sensor_name):
+		found = False
+		for key in self.sensor_dict.keys():
+			if sensor_name == key:
+				found = True
+				break
+		if not found:
+			print "Sensor does not exist"
+		else:		
+			print sensor_name + ":"
+			value_index = 0;
+			for value in self.sensor_dict[sensor_name]:
+				print "  value " + str(value_index) + " = " + str(value);
+				value_index = value_index + 1;
+			print
+
+	#prints the entire dictionary
+	def printDictionary (self):
+		for sensor in self.sensor_dict:
+			self.printSensorValues(sensor)
