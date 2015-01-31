aeac6861c31ce29e3491fc559aaf1d53c01ad218
Mitchell Mellone <mellom3@rpi.edu>
2015-01-30 21:57:08 -0500

    Deleted unnecessary function to output sensor names.

--- Flask Test/sensor_buffer.py
+++ Flask Test/sensor_buffer.py
@@ -32,120 +32,6 @@ class SensorBuffer:
 		else:
 			print "Sensor does not exist"
 
-	#returns the list of values for a specified sensor
-	#param sensor_name is the name of sensor/dictionary key
-	def getSensor (self, sensor_name):
-		found = False
-		for key in self.sensor_dict.keys():
-			if sensor_name == key:
-				found = True
-				break
-		if found:
-			return self.sensor_dict[sensor_name]
-		else:
-			print "Sensor does not exist"
-
-	#returns and erases the last value of a specified sensor
-	#param sensor_name is the name of sensor/dictionary key
-	def getLastSensorValue (self, sensor_name):
-		found = False
-		for key in self.sensor_dict.keys():
-			if sensor_name == key:
-				found = True
-				break
-		if not found:
-			print "Sensor does not exist"
-		else:
-			return self.sensor_dict[sensor_name].pop()
-
-	#returns a specified value of a specified sensor
-	#does not change the sensor list
-	#param sensor_name is the name of sensor/dictionary key
-	#param value_index is the index of the value to be returned
-	def getSensorValue (self, sensor_name, value_index):
-		found = False
-		for key in self.sensor_dict.keys():
-			if sensor_name == key:
-				found = True
-				break
-		if not found:
-			print "Sensor does not exist"
-		elif value_index >= len(self.sensor_dict[sensor_name]):
-			print "Value index does not exist"
-		else:
-			return self.sensor_dict[sensor_name][value_index]
-
-	#prints all of the values of a specified sensor
-	#param sensor_name is the name of sensor/dictionary key
-	def printSensorValues (self, sensor_name):
-		found = False
-		for key in self.sensor_dict.keys():
-			if sensor_name == key:
-				found = True
-				break
-		if not found:
-			print "Sensor does not exist"
-		else:
-			print sensor_name + ":"
-			value_index = 0;
-			for value in self.sensor_dict[sensor_name]:
-				print "  value " + str(value_index) + " = " + str(value)
-				value_index = value_index + 1
-			print
-
-	#prints the entire dictionary
-	def printDictionary (self):
-		for sensor in self.sensor_dict:
-			self.printSensorValues(sensor)
-
-	#prints all the values of a specified sensor to a file
-	#param sensor_name is the name of the sensor to print
-	#param file_name is the name of the file to print to
-	def printSensorToFile (self, sensor_name, file_name):
-		f = open(file_name, 'w')
-		for key in self.sensor_dict.keys():
-			if sensor_name == key:
-				found = True
-				break
-		if not found:
-			f.write("Sensor does not exist")
-		else:
-			f.write(sensor_name + ":")
-			value_index = 0;
-			for value in self.sensor_dict[sensor_name]:
-				f.write("  value " + str(value_index) + " = " + str(value))
-				value_index = value_index + 1;
-				f.write('\n')
-
-	#prints the entire dictionary to a file
-	def printDictionaryToFile (self, file_name):
-		f = open(file_name, 'w')
-		for sensor_name in self.sensor_dict.keys():
-			f.write(sensor_name + ":")
-			value_index = 0;
-			for value in self.sensor_dict[sensor_name]:
-				f.write(" value " + str(value_index) + " = " + str(value))
-				value_index = value_index + 1;
-				f.write('\n')
-
-	#prints and erases the last sensor value to a file
-	def printLastSensorValueToFile (self, sensor_name, file_name):
-		f = open(file_name, 'w')
-		for key in self.sensor_dict.keys():
-			if sensor_name == key:
-				found = True
-				break
-		if not found:
-			f.write("Sensor does not exist")
-		else:
-			f.write(sensor_name + ": " + str(self.getLastSensorValue(sensor_name)))
-		f.write('\n')
-
-	#prints and erases the last value from all sensors to a file
-	def printAllLastSensorValuesToFile (self, file_name):
-		f = open(file_name, 'w')
-		for sensor_name in self.sensor_dict.keys():
-			f.write(sensor_name + ": " + str(self.getLastSensorValue(sensor_name)) + '\n')
 
 	#prints the last value from each sensor to a file with a time stamp, appending the file
 	#does NOT erase the last value from the sensor
@@ -158,4 +44,11 @@ class SensorBuffer:
 			lastIndex = len(self.sensor_dict[sensor_name]) - 1
 			if lastIndex >= 0:
 				f.write(str(self.sensor_dict[sensor_name][lastIndex]) + ' ')
+		f.write('\n')
+
+	#prints all of the sensor names in the order they are in the dictionary
+	def getSensors (self, file_name):
+		f = open(file_name, 'a')
+		for sensor_name in self.sensor_dict.keys():
+			f.write(sensor_name + ' ')
 		f.write('\n')
